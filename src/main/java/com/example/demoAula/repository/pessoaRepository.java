0d3089249b6bd3942cd68c933bd5b8111a5277fa
Ardih123 <joaoandrade888@gmail.com>
2022-03-16 19:14:01 +0000

    removed pessoa

--- src/main/java/com/example/demoAula/repository/pessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoAula.repository;
-
-import com.example.demoAula.model.Pessoa;
-import org.springframework.data.repository.CrudRepository;
-
-public interface pessoaRepository  extends CrudRepository<Pessoa, Long> {
-}
